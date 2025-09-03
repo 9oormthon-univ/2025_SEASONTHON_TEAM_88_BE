dbbc1d1956290b0ac2db55a03c8784f5c63e4729
강예린 <yerin7786@gmail.com>
2025-09-04 01:26:46 +0900

    ✨[Feat] 카테고리 및 필터 enum 추가

---
+++ src/main/java/com/eventory/server/domain/product/entity/enums/SpecialOption.java
@@ -0,0 +1,12 @@
+package com.eventory.server.domain.product.entity.enums;
+
+public enum SpecialOption {
+    HANDMADE,
+    CUSTOM_MADE,
+    ENGRAVING,
+    PHOTO_PRODUCT,
+    MESSAGE_CARD,
+    GIFT_WRAP,
+    CUSTOMIZABLE,
+    GIFT_SET
+}
