b5a4c658cc6f0522ad20cdbc0d4b8b13c9dc077f
강예린 <yerin7786@gmail.com>
2025-09-06 03:41:07 +0900

    ✨[Feat] Todo 진행 상태 변경 기능 구현

--- src/main/java/com/eventory/server/domain/party/service/TodoService.java
+++ src/main/java/com/eventory/server/domain/party/service/TodoService.java
@@ -1,7 +1,9 @@
 package com.eventory.server.domain.party.service;
 
 import com.eventory.server.domain.party.dto.request.CreateTodoRequest;
+import com.eventory.server.domain.party.dto.request.TodoCompleteRequest;
 import com.eventory.server.domain.party.dto.response.CreateTodoResponse;
+import com.eventory.server.domain.party.dto.response.TodoCompleteResponse;
 import com.eventory.server.domain.party.entity.Party;
 import com.eventory.server.domain.party.entity.Todo;
 import com.eventory.server.domain.party.repository.PartyRepository;
@@ -19,8 +21,15 @@ public class TodoService {
     private final TodoRepository todoRepository;
     private final PartyRepository partyRepository;
 
+    /**
+     * 투두 리스트 항목 추가
+     * @param userId
+     * @param createTodoRequest
+     * @return createTodoRequest (partyId, task)
+     */
     @Transactional
     public CreateTodoResponse createTodo(Long userId, CreateTodoRequest createTodoRequest) {
+        // Todo memberId 추가
         Long partyId = createTodoRequest.partyId();
         Party party = partyRepository.findById(partyId)
                 .orElseThrow(() -> new GeneralException(ErrorStatus.PARTY_NOT_FOUND));
@@ -39,5 +48,24 @@ public class TodoService {
 
         todoRepository.save(todo);
         return new CreateTodoResponse(partyId, todo.getId(), task);
+    }
+
+    /**
+     * 투두 리스트 항목 진행 상태 변경
+     * @param memberId
+     * @param todoId
+     * @param todoCompleteRequest
+     * @return todoCompleteRequest (todoId, isCompleted)
+     */
+    @Transactional
+    public TodoCompleteResponse updateTodoComplete(Long memberId, Long todoId, TodoCompleteRequest todoCompleteRequest) {
+        // Todo memberId 추가
+        Todo todo = todoRepository.findById(todoId)
+                .orElseThrow(() -> new GeneralException(ErrorStatus.TODO_NOT_FOUND));
+
+        Boolean completed = todoCompleteRequest.isCompleted();
+        todo.updateStatus(!completed);
+
+        return new TodoCompleteResponse(todo.getId(), todo.isCompleted());
     }
 }
