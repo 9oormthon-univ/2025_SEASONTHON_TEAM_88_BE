e10e08aed7d116efbd078acc7dc5977b3d7afc22
강예린 <yerin7786@gmail.com>
2025-09-06 03:02:12 +0900

    ♻️[Refactor] Todo 테이블 병합에 따른 엔티티 구조 수정

--- src/main/java/com/eventory/server/domain/party/entity/Todo.java
+++ src/main/java/com/eventory/server/domain/party/entity/Todo.java
@@ -1,6 +1,7 @@
 package com.eventory.server.domain.party.entity;
 
 import com.eventory.server.domain.common.BaseEntity;
+import com.eventory.server.domain.product.entity.Product;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -18,4 +19,15 @@ public class Todo extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "party_id", nullable = false)
     private Party party;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "task")
+    private String task;
+
+    @Builder.Default
+    @Column(name = "is_completed")
+    private boolean isCompleted = false;
 }
